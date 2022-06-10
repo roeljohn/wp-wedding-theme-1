ba3e286ac2f95abf6177b4e2c73b2ac9f8ce63fb
roeljohn <bobisroeljohn@gmail.com>
2022-06-10 09:58:26 +0800

    add comment in content.php

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -1,3 +1,6 @@
-<?php esc_html_e( 'template parts content.php', 'wp-default' ); ?>
-<?php the_title();?>
-<?php the_excerpt(); ?>
+
+<?php 
+/** Template parts content is just a reusable section of the website. */
+esc_html_e( 'template parts content.php', 'wp-default' ); 
+the_title();
+the_excerpt(); ?>
